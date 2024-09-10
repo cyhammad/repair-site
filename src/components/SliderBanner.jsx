f469673092095e0641e49b7ef95a88c5c8093070
cyhammad <hammad727272@gmail.com>
2024-09-11 03:45:33 +0500

    banner fix

--- src/components/SliderBanner.jsx
+++ src/components/SliderBanner.jsx
@@ -28,7 +28,7 @@ export function SliderBanner() {
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        <motion.p className="font-bold text-5xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
           The best appliances repair service <br /> in Dubai and Abu Dhabi
         </motion.p>
         <div className="flex items-center mt-5 gap-3">
