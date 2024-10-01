5e4d5ef501f02f483369d0e3f94bf487353e6dc5
99-Malik <74320681+99-Malik@users.noreply.github.com>
2024-10-02 02:10:22 +0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import FixedCallButtons from "@/components/FixedCallButtons";
+import Script from "next/script";
 
 const font = Lato({
   subsets: ["latin"],
@@ -19,6 +20,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        {/* Include gtag.js script */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16647164467" />
+
+        {/* Include gtag.js initialization script */}
+        <Script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config','AW-16647164467');
+        ` }} />
+
+{/*         <Script dangerouslySetInnerHTML={{
+          __html: `
+        function gtagSendEvent(url) {
+    var callback = function () {
+      if (typeof url === 'string') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion_event_contact', {
+      'event_callback': callback,
+      'event_timeout': 2000,
+      // <event_parameters>
+    });
+    return false;
+  }
+        ` }} /> */}
+      </head>
       <body className={`${font.className} flex flex-col`}>
         {children}
         <FixedCallButtons />
